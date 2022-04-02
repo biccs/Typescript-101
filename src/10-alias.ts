320069553aea606f4c81cf3297bf25b81c62cc66
biccs <biccsdev@gmail.com>
2022-04-01 22:51:47 -0700

    Added alias file

--- src/10-alias.ts
+++ src/10-alias.ts
@@ -1,1 +1,24 @@
-(() => {})();
+(() => {
+  //TS Alias allows us to create our own data types as below.
+  type Foo = string | number;
+  //Newly created data type can be used as Type Annotation.
+  let foo: Foo;
+
+    //Literal Types
+    //Allows us to create a type with fixed value, if you store
+    // a different value tham the ones stated it will pop an alert.
+    type Size = 'S' | 'M' | 'L' | 'XL';
+    let size: Size;
+
+  //Following function prints data depending on data type input.
+  function greeting(foo: Foo, size: Size){
+    if(typeof foo === 'string'){
+      console.log(`Hello ${foo.toUpperCase()}, you're size ${size}`);
+    } else {
+      console.log(`Number: ${foo.toFixed(1)}, size ${size}`)
+    }
+  }
+
+  greeting("victor", 'M');
+  greeting(56.342, 'L');
+})();
