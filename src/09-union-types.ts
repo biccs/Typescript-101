0d65b212dff2bce146c30254302f2dd656ceddd9
biccs <biccsdev@gmail.com>
2022-04-01 22:24:00 -0700

    Added union types file

--- src/09-union-types.ts
+++ src/09-union-types.ts
@@ -1,3 +1,22 @@
 (() => {
-  
+  //Union types allows us to specify the type or types a variable might be.
+  let foo: string | number;
+  foo = 123;
+  foo = "Hello";
+  //Not allowed as this type is not included in TS but in JS will work fine in certain escenarios.
+  foo = true;
+  console.log(foo);
+
+  //Following function prints data depending on data type input.
+  function greeting(myText: string | number){
+    if(typeof myText === 'string'){
+      console.log(`Hello ${myText.toUpperCase()}`);
+    } else {
+      console.log(`Number: ${myText.toFixed(1)}`)
+    }
+  }
+
+  greeting("victor");
+  greeting(56.342);
+
 })();
